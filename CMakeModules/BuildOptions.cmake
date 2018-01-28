b48768b4b488cea6983b48926eff74e66136957f
Xycaleth <alex@acslo.com>
2018-01-28 12:11:08 +0000

    Some more tidying up

--- CMakeModules/BuildOptions.cmake
+++ CMakeModules/BuildOptions.cmake
@@ -1,5 +1,5 @@
 include(CMakeDependentOption)
-option(BuildPortableVersion "Build portable version (does not read or write files from your user/home directory" OFF)
+option(BUILD_PORTABLE "Build portable version (does not read or write files from your user/home directory" OFF)
 
 option(BUILD_JAMP "Create Jedi Academy multi player projects"	ON)
 option(BUILD_JASP "Create Jedi Academy single player projects"	OFF)
@@ -20,15 +20,15 @@ cmake_dependent_option(BuildJK2SPEngine		"Create Jedi Outcast SP engine project"
 cmake_dependent_option(BuildJK2SPGame		"Create Jedi Outcast SP game project"				OFF "BUILD_JOSP" OFF)
 cmake_dependent_option(BuildJK2SPRdVanilla	"Create Jedi Outcast SP default renderer project"	OFF "BUILD_JOSP" OFF)
 
-option(BuildTests "Build unit tests (requires Boost)" OFF)
+option(BUILD_TESTS "Build unit tests (requires Boost)" OFF)
 
-cmake_dependent_option(BuildSymbolServer "Build WIP Windows Symbol Server (experimental and unused)" OFF "NOT WIN32 OR NOT MSVC" OFF)
+cmake_dependent_option(BUILD_SYMBOL_SERVER "Build WIP Windows Symbol Server (experimental and unused)" OFF "NOT WIN32 OR NOT MSVC" OFF)
 
-cmake_dependent_option(UseInternalOpenAL "Use bundled OpenAL"  ON "WIN32" OFF)
-cmake_dependent_option(UseInternalZlib   "Use bundled zlib"    ON "WIN32" OFF)
-cmake_dependent_option(UseInternalPNG    "Use bundled libpng"  ON "WIN32" OFF)
-cmake_dependent_option(UseInternalJPEG   "Use bundled libjpeg" ON "WIN32 OR APPLE" OFF)
-cmake_dependent_option(UseInternalSDL2   "Use bundled SDL2"    ON "WIN32" OFF)
+cmake_dependent_option(USE_INTERNAL_OPENAL "Use bundled OpenAL"  ON "WIN32" OFF)
+cmake_dependent_option(USE_INTERNAL_ZLIB   "Use bundled zlib"    ON "WIN32" OFF)
+cmake_dependent_option(USE_INTERNAL_PNG    "Use bundled libpng"  ON "WIN32" OFF)
+cmake_dependent_option(USE_INTERNAL_JPEG   "Use bundled libjpeg" ON "WIN32 OR APPLE" OFF)
+cmake_dependent_option(USE_INTERNAL_SDL2   "Use bundled SDL2"    ON "WIN32" OFF)
 
 if(APPLE)
 	option(MakeApplicationBundles "Build .app bundles for executables" ON)
--- CMakeModules/GlobalDefinitions.cmake
+++ CMakeModules/GlobalDefinitions.cmake
@@ -30,7 +30,7 @@ endif()
 
 
 # Settings
-if(BuildPortableVersion)
+if(BUILD_PORTABLE)
 	list(APPEND SharedDefines "_PORTABLE_VERSION")
 endif()
 
