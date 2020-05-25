873415e3edc5d5b92e3e3fdb696d049c3fc1e365
Xycaleth <xycaleth@gmail.com>
2020-05-25 10:55:43 +0100

    Remove unused function

--- codemp/rd-strawberry/tr_init.cpp
+++ codemp/rd-strawberry/tr_init.cpp
@@ -427,10 +427,6 @@ static void InitVulkan( void )
 		GpuContextInit(gpuContext);
 
 		R_Splash();
-#if 0
-		// set default state
-		GL_SetDefaultState();
-#endif
 	}
 }
 
@@ -904,51 +900,6 @@ const void *RB_TakeVideoFrameCmd( const void *data )
 }
 
 //============================================================================
-
-/*
-** GL_SetDefaultState
-*/
-void GL_SetDefaultState( void )
-{
-	qglClearDepth( 1.0f );
-
-	qglCullFace(GL_FRONT);
-
-	qglColor4f (1,1,1,1);
-
-	// initialize downstream texture unit if we're running
-	// in a multitexture environment
-	if ( qglActiveTextureARB ) {
-		GL_SelectTexture( 1 );
-		GL_TextureMode( r_textureMode->string );
-		GL_TexEnv( GL_MODULATE );
-		qglDisable( GL_TEXTURE_2D );
-		GL_SelectTexture( 0 );
-	}
-
-	qglEnable(GL_TEXTURE_2D);
-	GL_TextureMode( r_textureMode->string );
-	GL_TexEnv( GL_MODULATE );
-
-	qglShadeModel( GL_SMOOTH );
-	qglDepthFunc( GL_LEQUAL );
-
-	// the vertex array is always enabled, but the color and texture
-	// arrays are enabled and disabled around the compiled vertex array call
-	qglEnableClientState (GL_VERTEX_ARRAY);
-
-	//
-	// make sure our GL state vector is set correctly
-	//
-	glState.glStateBits = GLS_DEPTHTEST_DISABLE | GLS_DEPTHMASK_TRUE;
-
-	qglPolygonMode (GL_FRONT_AND_BACK, GL_FILL);
-	qglDepthMask( GL_TRUE );
-	qglDisable( GL_DEPTH_TEST );
-	qglEnable( GL_SCISSOR_TEST );
-	qglDisable( GL_CULL_FACE );
-	qglDisable( GL_BLEND );
-}
 
 /*
 ================
